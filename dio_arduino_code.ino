62abc2d835aa2214681c2503f0437d19281d342a
Rafi59 <rafi59_dev@srvmaison.fr.nf>
2017-08-01 11:00:26 +0200

    Update dio_arduino_code.ino

--- dio_arduino_code.ino
+++ dio_arduino_code.ino
@@ -79,10 +79,7 @@ void setup() {
 
 void loop() {
  
-    //Turn ON HE300 
-  mySwitch.send(1234,0,true);
-  delay(2000);
-  if (mySwitch.available()) {
+if (mySwitch.available()) {
     int value = mySwitch.getReceivedValue();
     
     if (value == 0) {
